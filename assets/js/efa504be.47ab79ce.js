de617313304dd3878cfccab872a1219603d46707
sibelius <sibelius@users.noreply.github.com>
2022-11-22 01:15:58 +0000

    deploy: 0e8030049b6d95cdbfdc153dd7770a2d823586b5

---
+++ assets/js/efa504be.47ab79ce.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdev_docs=self.webpackChunkdev_docs||[]).push([[201],{3968:e=>{e.exports=JSON.parse('{"label":"culture","permalink":"/zettelkasten/tags/culture","allTagsPath":"/zettelkasten/tags","count":1,"items":[{"id":"culture","title":"Culture","description":"Culture is what people do when you are not looking","permalink":"/zettelkasten/culture"}]}')}}]);
