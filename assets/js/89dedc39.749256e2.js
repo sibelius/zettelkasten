28ccbf7424491dccedacc3f5c0fc8071ef9fe218
sibelius <sibelius@users.noreply.github.com>
2022-11-26 01:01:24 +0000

    deploy: dba364c398235ae5720b3f6db42a4299368a9f1a

---
+++ assets/js/89dedc39.749256e2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdev_docs=self.webpackChunkdev_docs||[]).push([[209],{5318:e=>{e.exports=JSON.parse('{"label":"ideas","permalink":"/zettelkasten/tags/ideas","allTagsPath":"/zettelkasten/tags","count":1,"items":[{"id":"ideas","title":"Ideas","description":"Grab any of these ideas and buidl","permalink":"/zettelkasten/ideas"}]}')}}]);
