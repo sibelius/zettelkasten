37268f1e3bc22cb68f048032e74fe1cc7033a050
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2022-11-29 23:27:00 -0300

    feat(articles): articles

--- src/Opengraph.tsx
+++ src/Opengraph.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import BrowserOnly from '@docusaurus/BrowserOnly';
+
+import { opengraphAppID } from './opengraphAppId';
+
 const Opengraph = (props) => {
   return (
     <BrowserOnly>
       {() => {
         const OpengraphReactComponent = require('opengraph-react').default;
         
-        return <OpengraphReactComponent {...props} />;
+        return <OpengraphReactComponent appId={opengraphAppID} size='small' {...props} />;
       }}
     </BrowserOnly>
   );
