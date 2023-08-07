3dbb0945e2d6c0cf06f518da1f8fd8a0bdeb6083
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2023-08-07 08:29:24 -0300

    feat(utterance): utterance

---
+++ src/useUtterances.tsx
@@ -0,0 +1,43 @@
+import { useEffect } from 'react';
+
+import { useScript } from './useScript';
+
+/**
+ * <script src="https://utteranc.es/client.js"
+ *               repo="sibelius/zettelkasten"
+ *               issue-term="pathname"
+ *               label="discussion"
+ *               theme="preferred-color-scheme"
+ *               crossOrigin="anonymous"
+ *               async>
+ *       </script>
+ */
+
+export const useUtterances = () => {
+  const scriptURL = 'https://utteranc.es/client.js';
+
+  const attributes = {
+    repo: "sibelius/zettelkasten",
+    'issue-term': "pathname",
+    label: "discussion",
+    theme: "preferred-color-scheme",
+    crossOrigin: "anonymous",
+    async: true,
+  }
+
+  // eslint-disable-next-line
+  const [loaded, error] = useScript({
+    src: scriptURL,
+    checkForExisting: false,
+    ...attributes,
+  });
+
+  useEffect(() => {
+    if (!error) {
+      return;
+    }
+
+    // eslint-disable-next-line
+    console.log('utteranc.es not loaded');
+  }, [error]);
+};
